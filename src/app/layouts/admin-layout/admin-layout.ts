20bcf0c111763e197be3c12916206f996c27e4b5
GKAYDER724 <155451281+KAYERTYK724@users.noreply.github.com>
2026-03-14 14:14:19 +0700

    setting theme

--- src/app/layouts/admin-layout/admin-layout.ts
+++ src/app/layouts/admin-layout/admin-layout.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
+import { AdminHeader } from "../../components/admin/admin-header/admin-header";
+import { Sidebar } from '../../components/admin/sidebar/sidebar';
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [],
+  imports: [AdminHeader, Sidebar, RouterOutlet],
   templateUrl: './admin-layout.html',
   styleUrl: './admin-layout.scss',
 })
