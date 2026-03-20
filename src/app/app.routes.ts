a4f9ce02249cbbcc7058bbfee7e2d4af8d62ad47
GKAYDER724 <155451281+KAYERTYK724@users.noreply.github.com>
2026-03-20 21:40:53 +0700

    edit admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,68 +4,71 @@ import { ClientLayout } from './layouts/client-layout/client-layout';
 
 export const routes: Routes = [
   {
-    path: "",
+    path: '',
     component: ClientLayout,
     children: [
       {
         path: '',
-        loadComponent: () => import('./pages/client/home/home').then(m => m.Home)
+        loadComponent: () => import('./pages/client/home/home').then((m) => m.Home),
       },
       {
         path: 'product',
-        loadComponent: () => import('./pages/client/product/product').then(m => m.Product)
+        loadComponent: () => import('./pages/client/product/product').then((m) => m.Product),
       },
       {
         path: 'product-detail/:id',
-        loadComponent: () => import('./pages/client/detail-product/detail-product').then(m => m.DetailProduct)
+        loadComponent: () =>
+          import('./pages/client/detail-product/detail-product').then((m) => m.DetailProduct),
       },
       {
         path: 'blog',
-        loadComponent: () => import('./pages/client/blog/blog').then(m => m.Blog)
+        loadComponent: () => import('./pages/client/blog/blog').then((m) => m.Blog),
       },
       {
         path: 'blog-detail',
-        loadComponent: () => import('./pages/client/detail-blog/detail-blog').then(m => m.DetailBlog)
+        loadComponent: () =>
+          import('./pages/client/detail-blog/detail-blog').then((m) => m.DetailBlog),
       },
       {
         path: 'about',
-        loadComponent: () => import('./pages/client/about/about').then(m => m.About)
+        loadComponent: () => import('./pages/client/about/about').then((m) => m.About),
       },
       {
         path: 'contact',
-        loadComponent: () => import('./pages/client/contact/contact').then(m => m.Contact)
+        loadComponent: () => import('./pages/client/contact/contact').then((m) => m.Contact),
       },
       {
         path: 'cart',
-        loadComponent: () => import('./pages/client/cart/cart').then(m => m.Cart)
+        loadComponent: () => import('./pages/client/cart/cart').then((m) => m.Cart),
       },
       {
         path: 'register',
-        loadComponent: () => import('./pages/client/register/register').then(m => m.Register)
+        loadComponent: () => import('./pages/client/register/register').then((m) => m.Register),
       },
       {
         path: 'login',
-        loadComponent: () => import('./pages/client/login/login').then(m => m.Login)
+        loadComponent: () => import('./pages/client/login/login').then((m) => m.Login),
       },
     ],
   },
   {
-  path: "admin",
-  component: AdminLayout,
-  children: [
-    {
-      path: '',
-      loadComponent: () => import('./pages/admin/dashboard/dashboard').then(m => m.Dashboard)
-    },
-    {
-      path: 'category',
-      loadComponent: () => import('./pages/admin/category/category').then(m => m.Category)
-    },
-     {
-        path: 'product', 
-        loadComponent: () => import('./pages/admin/product/product').then(m => m.Product)
-      }
-  ],
-},
+    path: 'admin',
+    component: AdminLayout,
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./pages/admin/dashboard/dashboard').then((m) => m.Dashboard),
+      },
+      {
+        path: 'category',
+        loadComponent: () => import('./pages/admin/category/category').then((m) => m.Category),
+      },
+      {
+        path: 'product',
+        loadComponent: () => import('./pages/admin/product/product').then((m) => m.Product),
+      },
+    ],
+  },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
