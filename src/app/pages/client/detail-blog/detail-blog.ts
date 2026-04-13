2ebcd504add73b6a6c4f0b5ae0f3e01e773b83c9
GKAYDER724 <155451281+KAYERTYK724@users.noreply.github.com>
2026-04-13 17:21:45 +0700

    ....

--- src/app/pages/client/detail-blog/detail-blog.ts
+++ src/app/pages/client/detail-blog/detail-blog.ts
@@ -1,9 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal, ChangeDetectorRef } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { BlogService } from '../../../services/blog.service';
+import { IBlog } from '../../../interfaces/blog.interface';
 
 @Component({
   selector: 'app-detail-blog',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './detail-blog.html',
   styleUrl: './detail-blog.scss',
 })
-export class DetailBlog {}
+export class DetailBlog implements OnInit {
+
+  blog: IBlog | null = null;
+  dataListBlog = signal<IBlog[]>([]);
+
+  prevBlog: IBlog | null = null;
+  nextBlog: IBlog | null = null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private blogService: BlogService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(async (params) => {
+      const id = Number(params.get('id'));
+      await this.fetchData(id);
+    });
+  }
+
+  async fetchData(id: number) {
+    try {
+      const [detail, list] = await Promise.all([
+        this.blogService.getById(id),
+        this.blogService.list()
+      ]);
+
+      if (detail.status === 200) {
+        this.blog = detail.data;
+        this.cdr.detectChanges();
+      }
+
+      if (list.status === 200) {
+        const blogs = list.data;
+        this.dataListBlog.set(blogs);
+
+        this.handlePrevNext(id, blogs);
+      }
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  handlePrevNext(id: number, blogs: IBlog[]) {
+    const index = blogs.findIndex(b => b.id === id);
+
+    this.prevBlog = index > 0 ? blogs[index - 1] : null;
+    this.nextBlog = index < blogs.length - 1 ? blogs[index + 1] : null;
+  }
+}
