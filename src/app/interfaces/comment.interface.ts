392037051719eac9bd616578031c03d29be945b8
GKAYDER724 <155451281+KAYERTYK724@users.noreply.github.com>
2026-04-15 14:41:11 +0700

    trang comments

---
+++ src/app/interfaces/comment.interface.ts
@@ -0,0 +1,15 @@
+import { IUser } from "./user.interface";
+
+export interface IComment {
+  id: number;
+  content: string;
+  date: string;        // timestamp -> string
+  status: number;      // tinyint -> number
+  product_id: number;
+  user_id: number;
+  product?: {
+    id: number;
+    name: string;
+  };
+  user?: IUser;
+}
